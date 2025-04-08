5c2e8166add7deda1075d4a72039a1758f63bd3f
down <17520320641@163.com>
2025-04-08 23:02:50 +0800

    git test 23:05

--- src/layout/components/Settings/index.vue
+++ src/layout/components/Settings/index.vue
@@ -73,6 +73,7 @@ export default {
   methods: {
     themeChange(val) {
       console.log(val)
+      console.log(val)
       this.$store.dispatch('settings/changeSetting', {
         key: 'theme',
         value: val
