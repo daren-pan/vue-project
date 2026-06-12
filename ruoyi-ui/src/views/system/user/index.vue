232fdbed838450a0046b302d17e037c9d2e5a7f5
caimf <caimf@huntkey.net>
2026-06-12 15:05:21 +0800

    修改了用户列表界面搜索按钮的文本，从“搜索1”改为“搜索2”。

--- ruoyi-ui/src/views/system/user/index.vue
+++ ruoyi-ui/src/views/system/user/index.vue
@@ -50,7 +50,7 @@
                   />
                 </el-form-item>
                 <el-form-item>
-                  <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索1</el-button>
+                  <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索2</el-button>
                   <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
                 </el-form-item>
               </div>
