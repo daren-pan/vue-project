8fb24c051d5f2d0b4d8a5c0a32e1de83aa644512
caimf <caimf@huntkey.net>
2026-06-15 13:46:35 +0800

    修改log内容

--- ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/controller/SysUserController.java
+++ ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/controller/SysUserController.java
@@ -85,7 +85,7 @@ public class SysUserController extends BaseController
     @GetMapping("/list")
     public TableDataInfo list(SysUser user)
     {
-        log.info("开始查询人员");
+        log.info("开始查询人员2");
         startPage();
         List<SysUser> list = userService.selectUserList(user);
         return getDataTable(list);
