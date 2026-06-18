96fbea77804bacad10830f007a9ee55962f8e4b0
caimf <caimf@huntkey.net>
2026-06-18 17:36:35 +0800

    修改log和dockerfile.system内容

--- ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/controller/SysUserController.java
+++ ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/controller/SysUserController.java
@@ -85,7 +85,7 @@ public class SysUserController extends BaseController
     @GetMapping("/list")
     public TableDataInfo list(SysUser user)
     {
-        log.info("开始查询人员列表，查询条件：{}", user);
+        log.info("开始查询人员列表，1");
         startPage();
         List<SysUser> list = userService.selectUserList(user);
         return getDataTable(list);
