e4829ffcc13aad75e8f1d3118a0e93866902690d
caimf <caimf@huntkey.net>
2026-07-07 17:11:59 +0800

    test: 测试增量构建检测

--- ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/controller/SysUserController.java
+++ ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/controller/SysUserController.java
@@ -85,7 +85,7 @@ public class SysUserController extends BaseController
     @GetMapping("/list")
     public TableDataInfo list(SysUser user)
     {
-        log.info("开始查询人员列表13");
+        log.info("开始查询人员列表1");
         startPage();
         List<SysUser> list = userService.selectUserList(user);
         return getDataTable(list);
