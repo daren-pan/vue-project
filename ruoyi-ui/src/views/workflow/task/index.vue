cd8f543290489141e21355f7e9eb92c1b26717f9
caimf <caimf@huntkey.net>
2026-08-06 15:33:24 +0800

    test: 前后端再改注释+标签，测试流水线

--- ruoyi-ui/src/views/workflow/task/index.vue
+++ ruoyi-ui/src/views/workflow/task/index.vue
@@ -23,7 +23,7 @@
       </el-col>
       <el-col :span="6">
         <span style="line-height:28px;font-size:12px;color:#666;">
-          颜色说明:<el-tag size="mini" type="warning" effect="plain">待审批</el-tag>
+          颜色说明1:<el-tag size="mini" type="warning" effect="plain">待审批</el-tag>
           <el-tag size="mini" type="info" effect="plain" style="margin-left:4px;">抄送</el-tag>
           <el-tag size="mini" type="danger" effect="plain" style="margin-left:4px;">会签</el-tag>
         </span>
